6b5f72b5486baa3c0e87f1e83bb101cebfd621ce
Toodie <lucas.vtodt@gmail.com>
2023-11-26 01:35:38 -0300

    add module for login related elements

---
+++ src/elements/login/login.js
@@ -0,0 +1,23 @@
+import './login-modal/login-modal.js';
+import './login-modal-heading/login-modal-heading.js';
+import './form-button/form-button.js';
+import './form-fields/form-fields.js';
+import { $body } from '../body/body.js';
+
+const LOGIN_MODAL_BODY = `
+<div id="login-modal">
+    <p id="login-modal-heading">Login</p>
+    <div class="form-field">
+        <label>Nome</label>
+        <input placeholder="Seu nome..." type="text" />
+    </div>
+    <div class="form-field">
+        <label>Senha</label>
+        <input placeholder="Sua senha..." type="password" />
+    </div>
+    <button id="form-btn">Entrar</button>
+</div>`;
+
+export function renderLoginModal() {
+    $body.html(LOGIN_MODAL_BODY);
+}
