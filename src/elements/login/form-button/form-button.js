adfe0f5bbd2c353ba31988f213940d4d2aeaea89
Toodie <lucas.vtodt@gmail.com>
2023-11-26 11:03:28 -0300

    add dummy access validator
    It´s basically compare the name and password provided
    by the user with the global DATABASE array then if
    it finds a match the user matched is saved in the global
    var LOGGED_USER for later usage, else a error message is shown.

--- src/elements/login/form-button/form-button.js
+++ src/elements/login/form-button/form-button.js
@@ -1,4 +1,19 @@
+import { nameInputValue, passwordInputValue } from '../form-fields/form-fields.js';
+import { DATABASE } from '../../../database/database.js';
+import { showAccessError } from '../access-error/access-errror.js';
+import { setLoggedUser } from '../login.js';
+
 const $formButton = $("#form-btn");
+
+function login() {
+    for(let user of DATABASE) {
+        if(user.name === nameInputValue() && user.password === passwordInputValue()) {
+            setLoggedUser(user);
+        } else {
+            showAccessError();
+        }
+    }
+}
 
 $formButton.css({
     "width": "100%",
@@ -9,3 +24,5 @@ $formButton.css({
     "cursor": "pointer",
     "margin-top": "52px"
 });
+
+$formButton.on("click", login);
