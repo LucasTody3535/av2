8fcd27ac2d800e2bab5cefab4376157a8a645a3e
Toodie <lucas.vtodt@gmail.com>
2023-11-26 01:34:05 -0300

    add $formButton element

---
+++ src/elements/login/form-button/form-button.js
@@ -0,0 +1,11 @@
+const $formButton = $("#form-btn");
+
+$formButton.css({
+    "width": "100%",
+    "height": "42px",
+    "border-radius": "5px",
+    "border": "none",
+    "background-color": "rgb(95, 95, 95)",
+    "cursor": "pointer",
+    "margin-top": "52px"
+});
